05aaeece3b8d80595ea9deb9db7511f50b03f605
rittripathi <tripathirit@gmail.com>
2026-07-31 02:33:28 +0530

    registry updated with aqi

--- app/trigger_handlers/registry.py
+++ app/trigger_handlers/registry.py
@@ -1,8 +1,10 @@
 # app/trigger_handlers/registry.py
 from app.trigger_handlers.crypto_price import CryptoPriceHandler
+from app.trigger_handlers.aqi import AQIHandler
 
 HANDLER_REGISTRY = {
     "PRICE": CryptoPriceHandler(),
+    "AQI": AQIHandler(),
 }
 
 
